e674360ea76216017b16eef5e4eb32777ca7d6cc
Manuelita13 <manuelitalanubecita@gmail.com>
2025-05-28 07:17:29 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+datos = []  # Variable global
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        edad = request.form['edad']
+        datos.append({'nombre': nombre, 'apellido': apellido, 'edad': edad})
+    return render_template('index.html', datos=datos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
